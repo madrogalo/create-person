d9d7888df4f0c3c2f45a857bf292efff26b9c8a3
Ievhenii <youn4ik@gmail.com>
2025-04-19 01:34:13 +0300

    add required field validation and error messages

--- src/components/Forms/CreatePersonForm/CreatePersonForm.tsx
+++ src/components/Forms/CreatePersonForm/CreatePersonForm.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import { Form, Field } from "react-final-form";
 
 import { FormHeader } from "../../FormHeader";
 import { InputField } from "../../InputField";
-import { Toggle } from "../../Toggle";
+import { required } from "../../../utils/validators";
 
 export function CreatePersonForm() {
   const onSubmit = (values: any) => {
@@ -16,21 +15,22 @@ export function CreatePersonForm() {
         onSubmit={onSubmit}
         render={({ handleSubmit }) => (
           <form onSubmit={handleSubmit}>
-            <div style={{ display: "flex", gap: "10px" }}>
+            <h2>Дані пацієнта</h2>
+            <div style={{ display: "flex", gap: "10px", paddingTop: "10px" }}>
               <InputField
-                label="Фамілія"
+                label="Прізвище"
                 name="surName"
                 component="input"
-                placeholder={"Фамілія"}
+                placeholder={""}
                 isRequired
+                validate={required}
               />
 
               <InputField
                 label="Ім'я"
-                name="Name"
+                name="name"
                 component="input"
                 placeholder={"Ім'я"}
-                isToggle
                 isRequired
               />
               <InputField
@@ -40,6 +40,42 @@ export function CreatePersonForm() {
                 placeholder={""}
                 isToggle
                 isRequired
+              />
+            </div>
+            <div style={{ display: "flex", gap: "10px" }}>
+              <InputField
+                name="identificationNumber"
+                component="input"
+                label={"РНОКПП (IПH)"}
+                placeholder={"РНОКПП (IПH)"}
+                isToggle
+                isRequired
+                validate={required}
+              />
+              <InputField
+                name="dateOfBirth"
+                component="input"
+                label={"Дата народження"}
+                placeholder={"01.10.1980"}
+                isRequired
+              />
+              <InputField
+                name="sex"
+                component="select"
+                label={"Стать"}
+                placeholder={"-- Вибрати --"}
+                isRequired
+                validate={required}
+                options={[
+                  {
+                    value: "male",
+                    label: "Чоловік",
+                  },
+                  {
+                    value: "female",
+                    label: "Жінка",
+                  },
+                ]}
               />
             </div>
             <div style={{ display: "flex", gap: "10px" }}>
@@ -56,44 +92,127 @@ export function CreatePersonForm() {
                 isRequired
               />
             </div>
-            <div style={{ display: "flex", gap: "10px" }}>
+            <div style={{ display: "flex", gap: "10px", paddingTop: "10px" }}>
               <InputField
                 name="preferredMethodCommunication"
                 label="Бажаний спосіб звʼязку із пацієнтом"
                 component="select"
-                placeholder={"Дата народження"}
+                placeholder={"-- Вибрати --"}
+                isRequired
+                validate={required}
+                options={[
+                  {
+                    value: "phone",
+                    label: "Телефон",
+                  },
+                  {
+                    value: "email",
+                    label: "Електронною поштою",
+                  },
+                ]}
+              />
+
+              <InputField
+                name="secretWord"
+                component="input"
+                placeholder={"Секретне слово (не менше 6 символів)"}
+                isRequired
+                validate={required}
+              />
+            </div>
+            <div style={{ display: "flex", gap: "10px", paddingTop: "10px" }}>
+              <InputField
+                name="phoneNumber"
+                component="input"
+                label={"Контактний номер телефону"}
+                placeholder={"+38 (_).__-__-__"}
+                isRequired
+                validate={required}
+              />
+              <InputField
+                name="email"
+                component="input"
+                label={"Адреса електронної пошти"}
+                placeholder={"example@example.com"}
+                isRequired
+                validate={required}
+              />
+            </div>
+            <h2>Документ, що посвідчує особу</h2>
+            <div style={{ display: "flex", gap: "10px", paddingTop: "10px" }}>
+              <InputField
+                name="typeOfDocument"
+                label="Тип документу"
+                component="select"
+                placeholder={"-- Вибрати --"}
+                isRequired
+                validate={required}
+                options={[
+                  {
+                    value: "identificationOfPerson",
+                    label:
+                      "Посвідчення особи, яка потребує додаткового захисту",
+                  },
+                  { value: "PassportIDCard", label: "Паспорт (ID-картка)" },
+                  { value: "PassportBooklet", label: "Паспорт (книжечка)" },
+                  {
+                    value: "PermanentResidencePermitInUkraine",
+                    label: "Посвідка на постійне проживання в Україні",
+                  },
+                  { value: "RefugeeCertificate", label: "Посвідка біженця" },
+                  {
+                    value: "ResidencePermit",
+                    label: "Посвідка на проживання",
+                  },
+                  {
+                    value: "TemporaryCertificateCitizenOfUkraine",
+                    label: "Тимчасове посвідчення громадянина України",
+                  },
+                ]}
+              />
+
+              <InputField
+                name="serialNumber"
+                component="input"
+                label={"Серія (за наявності), номер"}
+                placeholder={""}
+                isRequired
+                validate={required}
+              />
+            </div>
+            <div style={{ display: "flex", gap: "10px", paddingTop: "10px" }}>
+              <InputField
+                name="whenIssued"
+                component="input"
+                label={"Коли видано"}
+                placeholder={"31.12.1971"}
+                isRequired
+                validate={required}
+              />
+              <InputField
+                name="validUntil"
+                component="input"
+                label={"Діє до"}
+                placeholder={"31.12.1971"}
                 isRequired
               />
             </div>
-
-            {/* <div>
-              <label>First Name</label>
-              <Field
-                name="firstName"
+            <div style={{ display: "flex", gap: "10px", paddingTop: "10px" }}>
+              <InputField
+                name="issuedBy"
                 component="input"
-                placeholder="First Name"
+                label={"Ким видано"}
+                placeholder={""}
+                isRequired
+                validate={required}
+              />
+              <InputField
+                name="recordUNRD"
+                component="input"
+                label={"Запис Nº (УНЗР)"}
+                placeholder={"РРРРММДД-XXXXX"}
               />
             </div>
-            <div>
-              <label>Last Name</label>
-              <Field
-                name="lastName"
-                component="input"
-                placeholder="Last Name"
-              />
-            </div> */}
-            {/* <div>
-              <label>Last Name</label>
-              <Field
-                name="option"
-                component="select"
-                placeholder="----Last Name"
-              >
-                <option value="FF0000">Red</option>
-                <option value="00FF00">Green</option>
-                <option value="0000FF">Blue</option>
-              </Field>
-            </div> */}
             <button type="submit">Submit</button>
           </form>
         )}
--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,6 +1,6 @@
+// src/components/InputField/index.tsx
 import { FC } from "react";
-import { Form, Field } from "react-final-form";
-
+import { Field } from "react-final-form";
 import "./InputField.css";
 import { Toggle } from "../Toggle";
 
@@ -11,11 +11,16 @@ type InputFieldProps = {
   placeholder: string;
   isToggle?: boolean;
   isRequired?: boolean;
+  validate?: (value: any) => string | undefined;
+  options?: Array<{ value: string; label: string }>;
 };
+
 export const InputField: FC<InputFieldProps> = ({
   label,
   isToggle,
   isRequired,
+  validate,
+  options,
   ...props
 }) => {
   return (
@@ -25,7 +30,35 @@ export const InputField: FC<InputFieldProps> = ({
           {label} {isRequired && "*"}
         </label>
       )}
-      <Field {...props} />
+
+      <Field {...props} validate={validate}>
+        {({ input, meta }) => (
+          <>
+            {props.component === "select" ? (
+              <select {...input}>
+                <option value="" disabled selected>
+                  {props.placeholder}
+                </option>
+                {options?.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            ) : (
+              <input {...input} placeholder={props.placeholder} />
+            )}
+            <div style={{ height: 20 }}>
+              {meta.touched && meta.error && (
+                <div className="error" style={{ color: "red", fontSize: 12 }}>
+                  {meta.error}
+                </div>
+              )}
+            </div>
+          </>
+        )}
+      </Field>
+
       {isToggle && (
         <div className="toggle">
           <Toggle />
